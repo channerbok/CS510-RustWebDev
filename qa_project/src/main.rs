bacbcbc1e887bf5a31c3eeebc88e938b0f7693aa
channerbok <92491257+channerbok@users.noreply.github.com>
2024-04-24 01:08:19 -0700

    Populate from mock database using GET

--- qa_project/src/main.rs
+++ qa_project/src/main.rs
@@ -1,8 +1,15 @@
 /*
-Credit to Axum Documentation  - https://docs.rs/axum/latest/axum/
+Credit to Axum Documentation 
+ https://docs.rs/axum/latest/axum/
+ https://docs.rs/axum/latest/axum/extract/struct.State.html
+ https://docs.rs/tower-http/0.5.2/tower_http/cors/index.html
+
+ Credit to Tokio Documentation
+ https://github.com/tokio-rs/axum
 
 */
 
+use std::net::SocketAddr;
 use axum::http::{header, Method};
 use axum::Json;
 
@@ -11,10 +18,11 @@ use axum::{
     response::{IntoResponse, Response},
     routing::get,
     Router,
+    extract::State,
 
 };
 
-use std::sync::RwLock;
+use tokio::sync::RwLock;
 use std::sync::Arc;
 use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
@@ -35,15 +43,9 @@ impl Store{
         }
     }
 
-    /// Function to initialize the questions hashmap by reading in the questions from a json file
     fn init() -> HashMap<QuestionId, Question> {
         let file = include_str!("../questions.json");
-        let questions: HashMap<QuestionId, Question> =
-            serde_json::from_str::<HashMap<QuestionId, Question>>(file)
-                .unwrap()
-                .into_iter()
-                .collect();
-        questions
+        serde_json::from_str(file).expect("can't read questions.json")
     }
 
    
@@ -89,25 +91,19 @@ async fn handler_fallback() -> Response {
     (StatusCode::NOT_FOUND, "404 Not Found").into_response()
 }
 
-#[derive(Debug)]
-struct Pagination {
- start: usize,
- end: usize,
-}
 
 
 
 #[derive(Debug)]
 struct InvalidId;
 
-async fn get_questions(params: HashMap<String, String>, store: Arc<RwLock<HashMap<QuestionId, Question>>>) -> Result<Response<Json<Vec<Question>>>, StatusCode> {
+async fn get_questions( State(store): State<Store>) -> impl IntoResponse {
 
-    let guard = store.read().unwrap();
-
-
-    let questions: Vec<Question> = guard.values().cloned().collect();
-
-    Ok(Response::new(Json(questions)))
+    let questions= store.questions.read().await;
+    Response::builder()
+            .status(StatusCode::OK)
+            .body(serde_json::to_string_pretty(&questions.clone()).unwrap())
+            .unwrap()
 }
 
 
@@ -122,12 +118,14 @@ async fn main() {
         .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE]);
 
     let app = Router::new()
-       // .route("/questions", get(get_questions))
+        .route("/questions", get(get_questions))
         .layer(cors)
         .with_state(store)
         .fallback(handler_fallback);
         
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let ip = SocketAddr::new([127, 0, 0, 1].into(), 3000);
+    let listener = tokio::net::TcpListener::bind(ip).await.unwrap();
+    tracing::debug!("serving {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
