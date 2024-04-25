3f0d937fb0ed3d5483520a3b42aec8dbf4748283
channerbok <92491257+channerbok@users.noreply.github.com>
2024-04-24 21:29:38 -0700

    Added Error handling

--- qa_project/src/main.rs
+++ qa_project/src/main.rs
@@ -1,5 +1,5 @@
 /*
-Credit to Axum Documentation 
+Credit to Axum Documentation
  https://docs.rs/axum/latest/axum/
  https://docs.rs/axum/latest/axum/extract/struct.State.html
  https://docs.rs/tower-http/0.5.2/tower_http/cors/index.html
@@ -9,34 +9,32 @@ Credit to Axum Documentation
 
 */
 
-use std::net::SocketAddr;
 use axum::http::{header, Method};
-use axum::Json;
+use std::net::SocketAddr;
 
+use axum::body::Body;
 use axum::{
+    extract::State,
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::get,
     Router,
-    extract::State,
-
 };
-
-use tokio::sync::RwLock;
-use std::sync::Arc;
-use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::io::{Error, ErrorKind};
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 use std::str::FromStr;
 use tower_http::cors::{Any, CorsLayer};
 
 #[derive(Clone)]
 struct Store {
- questions: Arc<RwLock<HashMap<QuestionId, Question>>>,
+    questions: Arc<RwLock<HashMap<QuestionId, Question>>>,
 }
 
-impl Store{
+impl Store {
     fn new() -> Self {
         Store {
             questions: Arc::new(RwLock::new(self::Store::init())),
@@ -47,9 +45,6 @@ impl Store{
         let file = include_str!("../questions.json");
         serde_json::from_str(file).expect("can't read questions.json")
     }
-
-   
-    
 }
 
 #[allow(dead_code)]
@@ -64,16 +59,6 @@ struct Question {
 #[allow(dead_code)]
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq, Eq, Hash)]
 struct QuestionId(String);
-impl Question {
-    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
-        Question {
-            id,
-            title,
-            content,
-            tags,
-        }
-    }
-}
 
 impl FromStr for QuestionId {
     type Err = std::io::Error;
@@ -85,33 +70,69 @@ impl FromStr for QuestionId {
     }
 }
 
-
-
 async fn handler_fallback() -> Response {
     (StatusCode::NOT_FOUND, "404 Not Found").into_response()
 }
 
+// Handler to get questions 
+async fn get_questions(State(store): State<Store>) -> Result<Response, MyError> {
+    let questions = store.questions.read().await;
+    let error_param = MyError::MissingParameters;
+    let error_parse = MyError::ParseError;
 
+    
+    // Error handle for missing parameters
+    if questions.is_empty() {
+        return Err(error_param);
+    }
 
+    // Error handle for parser fail
+    let _json_string = match serde_json::to_string_pretty(&*questions) {
+        Ok(_json_string) => _json_string,
+        Err(_) => {
+            return Err(error_parse);
+        }
+    };
 
-#[derive(Debug)]
-struct InvalidId;
+    // Return the json response
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .body(Body::from(
+            serde_json::to_string_pretty(&*questions).unwrap(),
+        ))
+        .unwrap();
 
-async fn get_questions( State(store): State<Store>) -> impl IntoResponse {
-
-    let questions= store.questions.read().await;
-    Response::builder()
-            .status(StatusCode::OK)
-            .body(serde_json::to_string_pretty(&questions.clone()).unwrap())
-            .unwrap()
+    Ok(response)
 }
 
 
+// Custom Error type
+#[derive(Debug)]
+enum MyError {
+    ParseError,
+    MissingParameters,
+}
+
+// Custom error type implementation
+impl IntoResponse for MyError {
+    fn into_response(self) -> Response {
+        match self {
+            MyError::ParseError => Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from("Failed to parse integer"))
+                .unwrap(),
+            MyError::MissingParameters => Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(Body::from("Missing parameters"))
+                .unwrap(),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let store = Store::new();
-    
+
     let cors = CorsLayer::new()
         .allow_origin(Any)
         .allow_headers([header::CONTENT_TYPE])
@@ -122,7 +143,6 @@ async fn main() {
         .layer(cors)
         .with_state(store)
         .fallback(handler_fallback);
-        
 
     let ip = SocketAddr::new([127, 0, 0, 1].into(), 3000);
     let listener = tokio::net::TcpListener::bind(ip).await.unwrap();
